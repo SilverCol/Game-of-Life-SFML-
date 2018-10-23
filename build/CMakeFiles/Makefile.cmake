b1cb2570b6cc816100e252488c0baa97cf0e319b
Mitja Vodnik <mitjavodnik@gmail.com>
2018-10-23 20:17:59 +0200

    played with different events

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -42,5 +42,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/run.dir/DependInfo.cmake"
+  "CMakeFiles/events.dir/DependInfo.cmake"
   )
---
+++ src/events.cpp
@@ -0,0 +1,130 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktop, "SFML works!");
+    window.setVerticalSyncEnabled(true);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::Resized)
+            {
+                std::cout << "new width: " << event.size.width << std::endl;
+                std::cout << "new height: " << event.size.height << std::endl;
+            }
+
+            if (event.type == sf::Event::LostFocus)
+            {
+                std::cout << "Focus lost." << std::endl;
+            }
+            
+            if (event.type == sf::Event::GainedFocus)
+            {
+                std::cout << "Focus gained." << std::endl;
+            }
+
+            if (event.type == sf::Event::TextEntered)
+            {
+                if (event.text.unicode < 128)
+                    std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl;
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    std::cout << "the escape key was pressed" << std::endl;
+                    std::cout << "control:" << event.key.control << std::endl;
+                    std::cout << "alt:" << event.key.alt << std::endl;
+                    std::cout << "shift:" << event.key.shift << std::endl;
+                    std::cout << "system:" << event.key.system << std::endl;
+                }
+            }
+
+            if (event.type == sf::Event::MouseWheelScrolled)
+            {
+                if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+                    std::cout << "wheel type: vertical" << std::endl;
+                else if (event.mouseWheelScroll.wheel == sf::Mouse::HorizontalWheel)
+                    std::cout << "wheel type: horizontal" << std::endl;
+                else
+                    std::cout << "wheel type: unknown" << std::endl;
+                std::cout << "wheel movement: " << event.mouseWheelScroll.delta << std::endl;
+                std::cout << "mouse x: " << event.mouseWheelScroll.x << std::endl;
+                std::cout << "mouse y: " << event.mouseWheelScroll.y << std::endl;
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Right)
+                {
+                    std::cout << "the right button was pressed" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+                if (event.mouseButton.button == sf::Mouse::Middle)
+                {
+                    std::cout << "the middle button was pressed" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    std::cout << "the left button was pressed" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonReleased)
+            {
+                if (event.mouseButton.button == sf::Mouse::Right)
+                {
+                    std::cout << "the right button was released" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+                if (event.mouseButton.button == sf::Mouse::Middle)
+                {
+                    std::cout << "the middle button was released" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    std::cout << "the left button was released" << std::endl;
+                    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                }
+            }
+
+            if (event.type == sf::Event::MouseMoved)
+            {
+                std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
+                std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+            }
+
+            if (event.type == sf::Event::MouseEntered)
+                std::cout << "the mouse cursor has entered the window" << std::endl;
+            
+            if (event.type == sf::Event::MouseLeft)
+                std::cout << "the mouse cursor has left the window" << std::endl;
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
